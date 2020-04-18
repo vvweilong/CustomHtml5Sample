25af6ef69f2f43840863911258a1bfdc7aeddfad
zhuxiaolong <zhuxiaolong@ithro.com.cn>
2020-04-19 02:33:51 +0800

    init

--- app/src/main/assets/www/cordova_plugins.js
+++ app/src/main/assets/www/cordova_plugins.js
@@ -1,25 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-  module.exports = [
-        {
-        "id": "cordova-plugin-device.device",
-        "file": "plugins/cordova-plugin-device/www/device.js",
-        "pluginId": "cordova-plugin-device",
-        "clobbers": [
-          "device"
-        ]
-      },
-    {
-      "id": "cordova-plugin-dialogs.notification_android",
-      "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
-      "pluginId": "cordova-plugin-dialogs",
-      "merges": [
-        "navigator.notification"
-      ]
-    }
-  ];
+  module.exports = [ ];
   module.exports.metadata = {
-    "cordova-plugin-whitelist": "1.3.4",
-    "cordova-plugin-device": "2.0.3",
-    "cordova-plugin-dialogs": "2.0.2"
+    "cordova-plugin-whitelist": "1.3.4"
   };
 });
